d1426ab8d840707d1962b20a206be899383a10ef
Picces04 <khanhhaivodoi@gmail.com>
2025-07-28 11:32:32 +0700

    fix cookie axios

--- src/app/axios/api.js
+++ src/app/axios/api.js
@@ -1,15 +1,28 @@
 import axios from 'axios';
 
-// Lấy baseURL từ biến môi trường, mặc định là localhost:8000 nếu không có
+// Lấy baseURL từ biến môi trường, mặc định là URL production
 const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://reviewapp-be.onrender.com';
 
 const api = axios.create({
     baseURL,
-    withCredentials: true, // Gửi cookie lên server
     headers: {
         'ngrok-skip-browser-warning': 'true',
         'Content-Type': 'application/json',
     },
 });
+
+// Interceptor để thêm Bearer Token vào mỗi yêu cầu
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token'); // Lấy token từ localStorage
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 export default api;
