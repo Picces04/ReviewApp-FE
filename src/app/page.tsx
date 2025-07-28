f6aef11179eb0a43afeadb5a819e7c06842c2455
Picces04 <khanhhaivodoi@gmail.com>
2025-07-28 09:01:28 +0700

    fix Suspense "/"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next';
+import { Suspense } from 'react';
 import Header from './components/Header';
 import ReviewListApi from './components/ReviewListApi';
 
@@ -6,22 +7,31 @@ export const metadata: Metadata = {
     title: 'ReviewApp',
 };
 
+// Ngăn prerender nếu cần dùng hook client như usePathname, useSearchParams
+export const dynamic = 'force-dynamic';
+
 export default function Home() {
     return (
-        <>
-            <div className="main bg-[#E5EBFF] min-h-screen h-full  ">
-                <div className="max-w-[1280px] m-auto">
-                    <Header />
-                    <div className="min-h-[600px]">
+        <div className="main bg-[#E5EBFF] min-h-screen h-full">
+            <div className="max-w-[1280px] m-auto">
+                <Header />
+                <div className="min-h-[600px]">
+                    <Suspense
+                        fallback={
+                            <div className="text-center">
+                                Đang tải đánh giá...
+                            </div>
+                        }
+                    >
                         <ReviewListApi />
-                    </div>
-                    <div className="mt-10 left-1/3 right-1/3 footer flex items-center justify-center">
-                        <p className="text-sm text-gray-500 mt-auto">
-                            © 2025 ReviewAny. All rights reserved.
-                        </p>
-                    </div>
+                    </Suspense>
+                </div>
+                <div className="mt-10 left-1/3 right-1/3 footer flex items-center justify-center">
+                    <p className="text-sm text-gray-500 mt-auto">
+                        © 2025 ReviewAny. All rights reserved.
+                    </p>
                 </div>
             </div>
-        </>
+        </div>
     );
 }
